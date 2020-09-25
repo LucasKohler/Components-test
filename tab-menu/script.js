14a91c1926e4bec911c56316558faa11fcbea0aa
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2020-09-25 09:28:59 -0300

    melhorias no codigo css e adiçao de comentarios no script.js

--- tab-menu/script.js
+++ tab-menu/script.js
@@ -1,12 +1,14 @@
 const tabs = document.getElementsByClassName('tab');
 const content = document.getElementsByClassName('content');
 
+//Moving class selected to the new selected tab
 function tabStyle(tab) {
 	const selected = document.getElementsByClassName('selected');
 	selected[0].classList.toggle('selected');
 	tab.classList.toggle('selected');
 }
 
+//Moving the 'display: flex' to content with the same index as the selected tab
 function showContent(index) {
 	for (let i = 0; i < content.length; i++) {
 		content[i].style.display = 'none';
@@ -14,6 +16,7 @@ function showContent(index) {
 	content[index].style.display = 'flex';
 }
 
+//attaches an event handler to all tabs
 for (let i = 0; i < tabs.length; i++) {
 	tabs[i].addEventListener('click', () => {
 		tabStyle(tabs[i]);
